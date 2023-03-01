d750b27821a424b8d18a70e7d0b359a54026451a
JulesDrouet <jules.drouet44@gmail.com>
2023-03-01 15:48:17 +0100

    affiche l'historique des calculs
    affiche le panneau contenant l'historique des calculs effectués

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const screen = document.querySelector('.screen');
 //const buttons = document.querySelectorAll('.btn');
 const buttons = document.querySelectorAll('[data-num]');
 const equal = document.getElementById('btn-equal');
+const history = document.querySelector('.calculHistory');
 
 
 buttons.forEach(function (button) {
@@ -17,6 +18,15 @@ equal.addEventListener('click', function (e) {
         screen.value = '';
     } else {
         let answer = eval(screen.value);
+        let saveValue = screen.value + " = " +answer;
         screen.value = answer;
+
+        //On stock le calcul effectué dans une div qu'on ajoute au panel de droite
+        var newDiv = document.createElement("div");
+        newDiv.classList.add('calcul');
+        newDiv.textContent = saveValue;
+        history.appendChild(newDiv);
+
     }
 });
+
