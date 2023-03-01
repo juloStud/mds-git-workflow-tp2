1b2b1a1dda26c1dba5f47440fe13ec45b1fa0740
Alan <alandla00@gmail.com>
2023-03-01 15:32:49 +0100

    ajoute la possibilité de taper au clavier le calcul

--- index.js
+++ index.js
@@ -5,26 +5,44 @@ const equalBtn = document.getElementById('btn-equal');
 
 let beforeEqual = false;
 
-buttons.forEach(function (button) {
-    button.addEventListener('click', function (e) {
-        if (e.target.classList.contains('funcbtn')) {
-            if (beforeEqual) {
-                screenOldValue.value = screen.value;
-                beforeEqual = false;
-            } else {
-                const calcul = screenOldValue.value.toString() + screen.value.toString();
-                screenOldValue.value = eval(calcul);
-            }
-            screen.value = '';
+function writeElement(value) {
+    const regex = /^[/*+-]$/;
+    if (regex.test(value)) {
+        if (beforeEqual) {
+            screenOldValue.value = screen.value;
+            beforeEqual = false;
+        } else {
+            const calcul = screenOldValue.value.toString() + screen.value.toString();
+            screenOldValue.value = eval(calcul);
         }
-        let value = e.target.dataset.num;
-        screen.value += value;
-    })
-});
+        screen.value = '';
+    }
+    screen.value += value;
+}
 
-equalBtn.addEventListener('click', function (e) {
+function calculResult() {
     const calcul = screenOldValue.value.toString() + screen.value.toString();
     let answer = eval(calcul);
     beforeEqual = true
     screen.value = answer;
+}
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        writeElement(e.target.dataset.num);
+    })
 });
+
+equalBtn.addEventListener('click', function (e) {
+    calculResult();
+});
+
+document.addEventListener("keydown", function(event) {
+    const regex = /[0-9\/*+-.]/;
+    if (regex.test(event.key)) {
+      writeElement(event.key);
+    } else if (event.key === "Enter") {
+      calculResult();
+    }
+  });
+  
