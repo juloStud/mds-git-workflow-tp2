a752b3572536e8d07bae8379354f55b25a17c266
Alan <alandla00@gmail.com>
2023-03-01 14:39:39 +0100

    ajoute la possibilité de voir l'ancienne valeur de son calcul

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const screen = document.querySelector('.screen');
-//const buttons = document.querySelectorAll('.btn');
+const screen = document.getElementById('screen');
+const screenOldValue = document.getElementById('screen-value');
 const buttons = document.querySelectorAll('[data-num]');
-const equal = document.getElementById('btn-equal');
+const equalBtn = document.getElementById('btn-equal');
 
+let beforeEqual = false;
 
 buttons.forEach(function (button) {
     button.addEventListener('click', function (e) {
+        if (e.target.classList.contains('funcbtn')) {
+            if (beforeEqual) {
+                screenOldValue.value = screen.value;
+                beforeEqual = false;
+            } else {
+                const calcul = screenOldValue.value.toString() + screen.value.toString();
+                screenOldValue.value = eval(calcul);
+            }
+            screen.value = '';
+        }
         let value = e.target.dataset.num;
         screen.value += value;
     })
 });
 
-equal.addEventListener('click', function (e) {
-    console.log='equal'
-    if (screen.value == '') {
-        screen.value = '';
-    } else {
-        let answer = eval(screen.value);
-        screen.value = answer;
-    }
+equalBtn.addEventListener('click', function (e) {
+    const calcul = screenOldValue.value.toString() + screen.value.toString();
+    let answer = eval(calcul);
+    beforeEqual = true
+    screen.value = answer;
 });
